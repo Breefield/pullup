b2107a489c10d5acc9172928151b73ab063622a8
Dustin Hoffman <dustin.hoffman@breefield.com>
2014-02-11 11:18:49 -0800

    Outlines on buttons are ugly, and the upvote chevrons have them.

--- config/userlist.js
+++ config/userlist.js
@@ -1,5 +1,6 @@
 module.exports = {
-    users: [ // ADD YOUR USERNAME AT THE TOP
+    users: [ // ADD YOUR CASE SENSITIVE USERNAME AT THE TOP
+        'Breefield',
         'mattclaw',
         'crankeye',
         'cgroner',
